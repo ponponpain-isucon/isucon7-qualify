9a93e18a8ead327118913afa64ffa4410746209a
funwarioisii <mottirioisii@gmail.com>
2020-09-05 19:32:22 +0900

    use created_at in get_message

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -202,7 +202,7 @@ def get_message():
     select m.m_id, name, display_name, avatar_icon, m.created_at as created_at
     from user
         inner join (
-            select message.id as m_id, message.user_id
+            select message.id as m_id, message.user_id, message.created_at
             from message
             where message.id > {last_message_id} and channel_id = {channel_id}
             order by message.id desc
